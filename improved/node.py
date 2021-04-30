5f1b5bdc5bcc3a87a230fa47feac3690e5a19bf5
MirceaMelinte <mircea_melinte@hotmail.com>
2021-04-30 13:04:03 +0200

    Message complexity improvement tenatitve

---
+++ improved/node.py
@@ -0,0 +1,163 @@
+from flask import Flask, request, jsonify
+from util_methods import register_service, get_ports_of_nodes, generate_node_id, get_top_node, elect_top_node, \
+    announce, ready_for_election, get_details, check_health_of_the_service, check_node_available, get_free_port_and_node_name
+from bully import Bully
+import threading
+import time
+import random
+import sys
+import requests
+from multiprocessing import Value
+import logging
+
+
+counter = Value('i', 0)
+app = Flask(__name__)
+
+# # verifying if port number and node name have been entered as command line arguments.
+# port_number = int(sys.argv[1])
+# assert port_number
+
+# node_name = sys.argv[2]
+# assert node_name
+
+port_number, node_name = get_free_port_and_node_name()
+
+
+# saving the API logs to a file
+logging.basicConfig(filename=f"logs/{node_name}.log", level=logging.INFO)
+
+# an array to capture the messages that receive from acceptors
+learner_result_array = []
+
+node_id = generate_node_id()
+
+
+bully = Bully(node_name, node_id, port_number, port_number)
+
+# register service in the Service Registry
+service_register_status = register_service(node_name, port_number, node_id)
+
+
+def init(wait=True):
+    if service_register_status == 200:
+        ports_of_all_nodes = get_ports_of_nodes()
+        del ports_of_all_nodes[node_name]
+
+        # exchange node details with each node
+        node_details = get_details(ports_of_all_nodes)
+
+        if wait:
+            timeout = random.randint(5, 15)
+            print('timeouting in %s seconds' % timeout)
+            print('my node id: %s' % node_id)
+            time.sleep(timeout)
+
+        if len(node_details) == 0:
+            bully.coordinator = True
+            print('Coordinator is : %s' % node_name)
+        else:
+            # checks if there is an election on going
+            election_ready = ready_for_election(ports_of_all_nodes, bully.election, bully.coordinator)
+            
+            if election_ready or not wait:
+                print('Starting election in: %s' % node_name)
+                bully.election = True
+                top_node = get_top_node(node_details, node_id)
+                if top_node is None:
+                    bully.coordinator = True
+                    bully.election = False
+                    announce(node_name)
+                    print('Coordinator is : %s' % node_name)
+                    print('**********End of election**********************')
+                else:
+                    elect_top_node(top_node, node_id)
+    else:
+        print('Service registration is not successful')
+
+
+# this api is used to exchange details with each node
+@app.route('/nodeDetails', methods=['GET'])
+def get_node_details():
+    coordinator_bully = bully.coordinator
+    node_id_bully = bully.node_id
+    election_bully = bully.election
+    node_name_bully = bully.node_name
+    port_number_bully = bully.port
+    return jsonify({'node_name': node_name_bully, 'node_id': node_id_bully, 'coordinator': coordinator_bully,
+                    'election': election_bully, 'port': port_number_bully}), 200
+
+
+'''
+This API checks if the incoming node ID is grater than its own ID. If it is, it executes the init method and 
+sends an OK message to the sender. The execution is handed over to the current node. 
+'''
+
+
+@app.route('/response', methods=['POST'])
+def response_node():
+    data = request.get_json()
+    incoming_node_id = data['node_id']
+    self_node_id = bully.node_id
+    if self_node_id > incoming_node_id:
+        threading.Thread(target=init, args=[False]).start()
+        bully.election = False
+    return jsonify({'Response': 'OK'}), 200
+
+
+# This API is used to announce the coordinator details.
+@app.route('/announce', methods=['POST'])
+def announce_coordinator():
+    data = request.get_json()
+    coordinator = data['coordinator']
+    bully.coordinator = coordinator
+    print('Coordinator is %s ' % coordinator)
+    return jsonify({'response': 'OK'}), 200
+
+
+'''
+When nodes are sending the election message to the higher nodes, all the requests comes to this proxy. As the init
+method needs to execute only once, it will forward exactly one request to the responseAPI. 
+'''
+
+
+@app.route('/proxy', methods=['POST'])
+def proxy():
+    with counter.get_lock():
+        counter.value += 1
+        unique_count = counter.value
+
+    url = 'http://localhost:%s/response' % port_number
+    if unique_count == 1:
+        data = request.get_json()
+        requests.post(url, json=data)
+
+    return jsonify({'Response': 'OK'}), 200
+
+@app.route('/')
+def index():
+    return jsonify({'response': 'OK'}), 200
+
+
+# No node spends idle time, they always checks if the master node is alive in each 60 seconds.
+def check_coordinator_health():
+    threading.Timer(20.0, check_coordinator_health).start()
+    health = check_health_of_the_service(bully.coordinator)
+    if not health:
+        print('Coordinator unreachable. Starting election.')
+        init(False)
+    else:
+        print('Coordinator is alive')
+
+
+timer_thread1 = threading.Timer(2, init)
+timer_thread1.start()
+
+timer_thread2 = threading.Timer(20, check_coordinator_health)
+timer_thread2.start()
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=port_number)
+
+
+
---
+++ improved/util_methods.py
@@ -0,0 +1,168 @@
+import time
+import json
+import requests
+from random import randint
+import socket
+
+portRange = range(5000, 6000)
+
+def generate_node_id():
+    millis = int(round(time.time() * 1000))
+    node_id = millis + randint(800000000000, 900000000000)
+    return node_id
+
+
+# This method is used to register the service in the service registry
+def register_service(name, port, node_id):
+    print(f"registered service, {name}, {port}")
+
+    # derigster unnecessary services first
+    ids = get_list_of_node_ids(port)
+    print("found ids:", ids)
+    url = "http://localhost:8500/v1/agent/service/deregister/"
+    for i in ids:
+        requests.put(url + str(i))
+
+    url = "http://localhost:8500/v1/agent/service/register?replace-existing-checks=true"
+    data = {
+        "Name": name,
+        "ID": str(node_id),
+        "port": port,
+        "check": {
+            "name": "Check Counter health %s" % port,
+            "tcp": "localhost:%s" % port,
+            "interval": "10s",
+            "timeout": "1s"
+        }
+    }
+    put_request = requests.put(url, json=data)
+    return put_request.status_code
+
+def check_node_available(port):
+    # url = 'http://localhost:%s/' % port
+    # post_response = requests.post(url)
+    # if 200 == post_response:
+    #     return 200
+    # return 404
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        res = sock.connect_ex(('localhost', port))
+        if res == 0:
+            return True
+        else:
+            return False
+
+def get_free_port_and_node_name():
+    used_ports = []
+    ports = get_ports_of_nodes()
+    for n, p in ports.items():
+        if check_node_available(p):
+            used_ports.append(p)
+    # get first unused port in port range
+    p = list(filter(lambda x: x not in used_ports, portRange))
+    if len(p) < 1:
+        print("error: no port free in range.")
+        exit(1)
+    else: 
+        p = p[0]
+    return p, "node" + str(p-5000)
+
+def check_health_of_the_service(service):
+    print('Checking health of the %s' % service)
+
+    # url = 'http://localhost:8500/v1/agent/health/service/name/%s' % service
+    # response = requests.get(url)
+    # response_content = json.loads(response.text)
+    # aggregated_state = response_content[0]['AggregatedStatus']
+    # service_status = aggregated_state
+    # if response.status_code == 503 and aggregated_state == 'critical':
+    #     service_status = 'crashed'
+    # print('Service status: %s' % service_status)
+    # return service_status
+
+    return service in get_ports_of_nodes().keys()
+
+
+
+# get ports of all the registered nodes from the service registry
+def get_ports_of_nodes():
+    ports_dict = {}
+    response = requests.get('http://127.0.0.1:8500/v1/agent/services')
+    nodes = json.loads(response.text)
+    for each_service in nodes:
+        service = nodes[each_service]['Service']
+        status = nodes[each_service]['Port']
+        key = service
+        value = status
+        if check_node_available(value):
+            ports_dict[key] = value
+    return ports_dict
+
+def get_list_of_node_ids(port):
+    id_list = []
+    response = requests.get('http://127.0.0.1:8500/v1/agent/services')
+    nodes = json.loads(response.text)
+    for each_service in nodes:
+        id = nodes[each_service]['ID']
+        if nodes[each_service]['Port'] == port:
+            id_list.append(id)
+
+    return id_list
+
+def get_top_node(node_details, node_id):
+    higher_node_array = []
+    for each in node_details:
+        if each['node_id'] > node_id:
+            higher_node_array.append(each['port'])
+    return None if len(higher_node_array) == 0 else max(higher_node_array)
+
+
+# this method is used to send the higher node id to the proxy
+def elect_top_node(top_node, node_id):
+    status_code_array = []
+    url = 'http://localhost:%s/proxy' % top_node
+    data = {
+        "node_id": node_id
+    }
+    post_response = requests.post(url, json=data)
+    return post_response.status_code
+
+
+# this method returns if the cluster is ready for the election
+def ready_for_election(ports_of_all_nodes, self_election, self_coordinator):
+    coordinator_array = []
+    election_array = []
+    node_details = get_details(ports_of_all_nodes)
+
+    for each_node in node_details:
+        coordinator_array.append(each_node['coordinator'])
+        election_array.append(each_node['election'])
+    coordinator_array.append(self_coordinator)
+    election_array.append(self_election)
+
+    if True in election_array or True in coordinator_array:
+        return False
+    else:
+        return True
+
+
+# this method is used to get the details of all the nodes by syncing with each node by calling each nodes' API.
+def get_details(ports_of_all_nodes):
+    node_details = []
+    for each_node in ports_of_all_nodes:
+        if check_node_available(ports_of_all_nodes[each_node]):
+            url = 'http://localhost:%s/nodeDetails' % ports_of_all_nodes[each_node]
+            data = requests.get(url)
+            node_details.append(data.json())
+    return node_details
+
+
+# this method is used to announce that it is the master to the other nodes.
+def announce(coordinator):
+    all_nodes = get_ports_of_nodes()
+    data = {
+        'coordinator': coordinator
+    }
+    for each_node in all_nodes:
+        url = 'http://localhost:%s/announce' % all_nodes[each_node]
+        print(url)
+        requests.post(url, json=data)
