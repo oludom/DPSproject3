20c2d7660f0d91f373e78461656b786edf49ae43
Micha Heiß <mhlauffen@web.de>
2021-04-30 21:32:46 +0200

    election kinda working

--- bully.py
+++ bully.py
@@ -1,10 +1,89 @@
+from consul_connector import ConsulService
+
+
 class Bully:
 
-    def __init__(self, node_name, node_id, port_number, election=False, coordinator=False):
-        self.node_name = node_name
-        self.node_id = node_id
-        self.port = port_number
-        self.election = election
-        self.coordinator = coordinator
+    # def __init__(self, node_name, node_id, port_number, election=False, coordinator=False):
+    #     self.node_name = node_name
+    #     self.node_id = node_id
+    #     self.port = port_number
+    #     self.election = election
+    #     self.coordinator = coordinator
+
+    def __init__(self):
+        self._connection = ConsulService()
+        self._election = False
+        self._coordinator = False
+        self._currentCoordinatorIp = self._connection._ip
+        
 
 
+    # join network
+    def join(self):
+        self.sendElectionMessage()
+
+    # sent to announce election
+    def sendElectionMessage(self):
+        print('bully.sendElectionMessage', flush=True)
+        self._election = True
+
+        nodes = self._connection.getNodeList()
+        print("nodesList:", nodes, flush=True)
+        nodes.remove(self._connection._ip)
+        print("nodesList:", nodes, flush=True)
+        # send to higher numbered nodes
+        higher = list(filter(lambda x: self.compareIpHigher(self._connection._ip, x) , nodes)) 
+        # if no higher nodes, you are leader
+        print("higher:", higher, flush=True)
+        if len(higher) < 1: 
+            self._announceCoordinator(nodes)
+        else:
+            # elect higher nodes
+            answers = False
+            for i in range(len(higher)):
+                answers = answers | self._connection.sendElectionMessage(higher[i])
+            # if no answer received, send victory message and become coordinator
+            if not answers:
+                self._announceCoordinator(nodes)
+            else:
+                return # don't do anything if you get an answer
+        # if answer from higher node received, don't send any more messages and wait for victory message
+        # if answer from lower node received, restart election
+        return 
+    
+    def receiveElectionMessage(self, ip):
+        print('bully.receiveElectionMessage', flush=True)
+        print("remote ip: ", ip, flush=True)
+        if self.compareIpHigher(self._connection._ip, ip):
+            # other is leader, wait for victory
+            self._election = False
+        else:
+            self.sendElectionMessage()
+    
+    # # answers election message (alive)
+    # def answer(self):
+    #     if self._election:
+    #         self._election= False
+
+    # coordinator message (victory)
+    def coordinator(self, ip):
+        print('bully.coordinator', flush=True)
+        self._coordinator = False
+        self._currentCoordinatorIp = ip
+        self._election = False
+
+    def compareIpHigher(self, one, two):
+        one = bytes(map(int, one.split('.')))
+        two = bytes(map(int, two.split('.')))
+        return one < two
+
+    def checkCoordinator(self):
+        print('bully.checkCoordinator', flush=True)
+        self._connection.checkNodeAvailable(self._currentCoordinatorIp)
+
+    def _announceCoordinator(self, nodes):
+        self._election = False
+        self._coordinator = True
+        # announce coordinator
+        for i in range(len(nodes)):
+            self._connection.sendCoordinatorMessage(nodes[i])
---
+++ connector.py
@@ -0,0 +1,72 @@
+import socket, requests, nslookup
+
+# config
+PORT = 5000
+CONSUL_SERVER_ADDRESS = 'http://localhost:8500'
+
+class ConnectionService():
+
+    def __init__(self):
+        self._name = socket.gethostname()
+        self._ip = socket.gethostbyname(self._name)
+
+
+    # returns list of nodes in cluster
+    def getNodeList(self):
+        n = nslookup.Nslookup().dns_lookup("headless-service.default.svc.cluster.local")
+        return n.answer
+        # return ['127.0.1.1', '127.0.1.2']
+
+    def registerNode(self, name):
+        print(f"registering service, {self._name}, {self._ip}")
+
+        # derigster unnecessary services first
+        ids = self.getNodeList()
+        print("found ids:", ids)
+        url = CONSUL_SERVER_ADDRESS + "/v1/agent/service/deregister/"
+        for i in ids:
+            requests.put(url + str(i))
+
+        url = CONSUL_SERVER_ADDRESS + "/v1/agent/service/register"
+        data = {
+            "Name": name,
+            # "ID": str(node_id),
+            "Address": self._ip,
+            "Port": PORT,
+            "Check": {
+                "Name": "Check Counter health %s" % PORT,
+                "tcp": str(self._ip) + ":%s" % PORT,
+                "interval": "10s",
+                "timeout": "1s",
+                "DeregisterCriticalServiceAfter": "5m"
+            }
+        }
+        put_request = requests.put(url, json=data)
+        return put_request.status_code
+
+
+    def sendElectionMessage(self, ip):
+        return self.checkNodeAvailable(ip, '/election')
+
+    def sendCoordinatorMessage(self, ip):
+        return self.checkNodeAvailable(ip, '/coordinator')
+
+    def checkNodeAvailable(self, ip, path=''):
+        print('consul_connector.checkNodeAvailable:', ip + ":" + str(PORT) + path, flush=True)
+        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        #     res = sock.connect_ex((ip + ":" + str(PORT) + path))
+        #     if res == 0:
+        #         return True
+        #     else:
+        #         return False
+        try:
+            res = requests.get('http://' + ip + ":" + str(PORT) + path)
+            res.raise_for_status()
+        except requests.exceptions.HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}', flush=True)
+        except Exception:
+            print("FALSE", flush=True)
+            return False
+        
+        print("TRUE", flush=True)
+        return True
